14bced1e042121bb564e6ab5019f420c5f8195ec
intelogroup <jimkalinov@gmail.com>
2024-11-03 09:57:50 -0500

    chore: simplify Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@babel/plugin-transform-react-jsx']
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 4000
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'heroicons': ['@heroicons/react/24/outline']
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    force: true,
-    include: [
-      'react',
-      'react-dom',
-      '@heroicons/react/24/outline'
-    ]
+    outDir: 'dist'
   }
 })
